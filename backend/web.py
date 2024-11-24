6fed4614f795b9cdf1b5df1e1983915f3cbd091f
kingkingyyk <king8793yyk@hotmail.com>
2024-11-24 10:40:11 +0800

    Add copy

--- backend/web.py
+++ backend/web.py
@@ -54,4 +54,4 @@ def serve_files(section: str, file: str):
     return send_file(ret_file, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run("0.0.0.0", debug=True)
